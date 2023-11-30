97bb7ba615c3304944a6d6910ddec5f7381524ee
조하담 <hadamcho97@gmail.com>
2023-11-30 16:58:22 +0900

    Register MemberRepository directly as a bean

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,14 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
