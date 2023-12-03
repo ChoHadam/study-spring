9ac85e546e898294f94f16dd6109a95e70d93d62
조하담 <hadamcho97@gmail.com>
2023-12-03 17:08:49 +0900

    Implement Get user list

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,10 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.dto.CreateMemberDto;
 import hello.hellospring.service.MemberService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -17,6 +21,13 @@ public class MemberController {
     @GetMapping("members/new")
     public String createForm() {
         return "members/createMemberForm";
+    }
+
+    @GetMapping("members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
     @PostMapping("members/new")
