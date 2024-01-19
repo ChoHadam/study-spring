94af7fded820548727dee1f550ede60469359ef4
조하담 <hadamcho97@gmail.com>
2024-01-19 14:58:57 +0900

    Create TimeTraceAop using AOP

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,8 @@
 package hello.hellospring;
 
+import hello.hellospring.app.TimeTraceAop;
 import hello.hellospring.repository.MemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -9,6 +11,11 @@ public class SpringConfig {
 
     public SpringConfig(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public TimeTraceAop timeTraceAop() {
+        return new TimeTraceAop();
     }
 
 //    @Bean
